518c0ffba326e939b3b3ccfed076689482106b80
LCUSTOS <lucas.custos@ynov.com>
2022-09-09 10:39:14 +0200

    setpivalue

---
+++ SetPiValue.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Text.RegularExpressions;
+
+namespace CSharpDiscovery.Quest01
+{
+    public class SetPiValue_Exercice
+    {
+        public static float SetPiValue(float pi)
+        {
+            return (float) Math.Round(Math.PI, 5);
+        }
+    }
+}
