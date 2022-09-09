18d7b8f976adcd771c7e575bc5e3b11ebaae3191
LCUSTOS <lucas.custos@ynov.com>
2022-09-09 11:08:53 +0200

    getstringsize

---
+++ GetStringSize.cs
@@ -0,0 +1,10 @@
+namespace CSharpDiscovery.Quest01
+{
+    public class GetStringSize_Exercice
+    {
+        public static int GetStringSize(string str)
+        {
+            return str.Length;
+        }
+    }
+}
