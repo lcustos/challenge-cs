1028cc1f92aeff45d2bef49eb1b55bcacdd6c798
LCUSTOS <lucas.custos@ynov.com>
2022-09-09 12:25:02 +0200

    NextTrafficLightColor.cs

---
+++ NextTrafficLightColor.cs
@@ -0,0 +1,20 @@
+using CSharpDiscovery.Models;
+
+namespace CSharpDiscovery.Quest01
+{
+    public class NextTrafficLightColor_Exercice
+    {
+        public static TrafficLightColor GetNextTrafficLightColor(TrafficLightColor currentColor)
+        {
+            if (currentColor == TrafficLightColor.Red) {
+                return TrafficLightColor.Green;
+            } else if (currentColor == TrafficLightColor.Orange) {
+                return TrafficLightColor.Red;
+            } else if (currentColor == TrafficLightColor.Green) {
+                return TrafficLightColor.Orange;
+            } else {
+                return TrafficLightColor.None;
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,14 +1,16 @@
 ﻿using System;
 using CSharpDiscovery.Quest01;
+using CSharpDiscovery.Models;
 
 namespace TestCSharp
 {
     class Program
     {
-        static void Main(string[] str)
+        static void Main(string[] args)
         {
-            Console.WriteLine(BeginsWithSpecificCharacter_Exercice.BeginsWithSpecificCharacter("helloworld", 'h'));
-            Console.WriteLine(BeginsWithSpecificCharacter_Exercice.BeginsWithSpecificCharacter("helloworld", 'w'));
+            Console.WriteLine(NextTrafficLightColor_Exercice.GetNextTrafficLightColor(TrafficLightColor.Red));
+            Console.WriteLine(NextTrafficLightColor_Exercice.GetNextTrafficLightColor(TrafficLightColor.Orange));
+            Console.WriteLine(NextTrafficLightColor_Exercice.GetNextTrafficLightColor(TrafficLightColor.None));
         }
     }
 }
