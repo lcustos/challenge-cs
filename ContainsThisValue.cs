99aa9da1aa46ee55c084c9bb268090cec9046052
LCUSTOS <lucas.custos@ynov.com>
2022-09-12 09:13:06 +0200

    ContainsThisValue.cs

---
+++ ContainsThisValue.cs
@@ -0,0 +1,12 @@
+using System.Linq;
+
+namespace CSharpDiscovery.Quest02
+{
+    public class ContainsThisValue_Exercice
+    {
+        public static bool ContainsThisValue(int[] tab, int value)
+        {
+            return tab.Contains(value);
+        }
+    }
+}
