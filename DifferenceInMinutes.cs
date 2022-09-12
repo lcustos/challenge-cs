4cc58f46b2d4805182776d6b484050d87edc64b2
LCUSTOS <lucas.custos@ynov.com>
2022-09-12 09:02:42 +0200

    DifferenceInMinutes.cs

---
+++ DifferenceInMinutes.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace CSharpDiscovery.Quest02
+{
+    public class DifferenceInMinutes_Exercice
+    {
+        public static double DifferenceInMinutes(DateTime start, DateTime end)
+        {
+            TimeSpan diff = end - start;
+            return diff.TotalMinutes;
+        }
+    }
+}
