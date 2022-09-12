d9174d83ba4f2b077d055d0f4ee7d46816da4ff8
LCUSTOS <lucas.custos@ynov.com>
2022-09-12 09:24:20 +0200

    SortTable.cs

---
+++ SortTable.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections;
+namespace CSharpDiscovery.Quest02
+{
+    public class SortTable_Exercice
+    {
+        public static int[] SortTable(int[] tab)
+        {
+            Array.Sort(tab);
+            return tab;
+        }
+    }
+}
