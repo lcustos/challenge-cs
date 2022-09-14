d4bd6a74a3e5e9fed0887c8472d35d05fcdefffa
LCUSTOS <lucas.custos@ynov.com>
2022-09-14 09:04:07 +0200

    PointOfInterest.cs

--- PointOfInterest.cs
+++ PointOfInterest.cs
@@ -2,9 +2,9 @@ namespace CSharpDiscovery.Quest03
 {
 public class PointOfInterest
 {
-    public double Latitude = 0.0;
-    public double Longitude = 0.0;
-    public string Name = "";
-    static public string GoogleMapsUrlTemplate = "https://www.google.com/maps/place/{0}/@{1},{2},15z/";
+    public static string GoogleMapsUrlTemplate { get; set; } = "https://www.google.com/maps/place/{0}/@{1},{2},15z/";
+    public double Latitude { get; set; } = 0.0;
+    public double Longitude { get; set; } = 0.0;
+    public string Name { get; set; } = "";
 }
 }
