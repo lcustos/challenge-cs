824844f080355bebe0769f5c998f42958e78cfc0
LCUSTOS <lucas.custos@ynov.com>
2022-09-14 09:07:20 +0200

    SetConstructors

--- PointOfInterest.cs
+++ PointOfInterest.cs
@@ -6,5 +6,16 @@ public class PointOfInterest
     public double Latitude { get; set; } = 0.0;
     public double Longitude { get; set; } = 0.0;
     public string Name { get; set; } = "";
+    public PointOfInterest() {
+        Name = "Bordeaux Ynov Campus";
+        Latitude = 44.854186;
+        Longitude = -0.5663056;
+    }
+    public PointOfInterest(string _name, double _latitude, double _longitude) {
+        Name = _name;
+        Latitude = _latitude;
+        Longitude = _longitude;
+    }
 }
+
 }
--- Program.cs
+++ Program.cs
@@ -7,12 +7,15 @@ namespace TestCSharp
     {
         static void Main(string[] str)
         {
-            var poi = new PointOfInterest();
-            Console.WriteLine(poi.Latitude);
-            Console.WriteLine(poi.Longitude);
-            Console.WriteLine(poi.Name);
+            var DefaultPoint = new PointOfInterest();
+            Console.WriteLine(DefaultPoint.Name);
+            Console.WriteLine(DefaultPoint.Latitude);
+            Console.WriteLine(DefaultPoint.Longitude);
 
-            Console.WriteLine(PointOfInterest.GoogleMapsUrlTemplate);
+            var ParisCampus = new PointOfInterest("Paris Ynov Campus", 48.9016552, 2.2079985);
+            Console.WriteLine(ParisCampus.Name);
+            Console.WriteLine(ParisCampus.Latitude);
+            Console.WriteLine(ParisCampus.Longitude);
         }
     }
 }
